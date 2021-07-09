504af5fe62f5fb26515ecfddef8cd68be399a28c
Sean Ye <yesean@amazon.com>
2021-07-09 16:04:45 -0700

    Add type annotations to parser

--- okpt/io/config/parser.py
+++ okpt/io/config/parser.py
@@ -1,10 +1,13 @@
+from io import TextIOWrapper
 import json
+from typing import Any, Dict
+
 import yaml
 
 
-def parse_yaml(file):
+def parse_yaml(file: TextIOWrapper) -> Dict[Any, Any]:
     return yaml.load(file, Loader=yaml.SafeLoader)
 
 
-def parse_json(file):
+def parse_json(file: TextIOWrapper) -> Dict[Any, Any]:
     return json.load(file)
