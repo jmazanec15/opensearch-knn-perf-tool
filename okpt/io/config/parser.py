130931d93962e9d1a9759d31b094d3547691351c
Sean Ye <yesean@amazon.com>
2021-07-12 18:29:05 -0700

    Add type annotations and parse from path

--- okpt/io/config/parser.py
+++ okpt/io/config/parser.py
@@ -3,11 +3,22 @@ import json
 from typing import Any, Dict
 
 import yaml
+from okpt.io import reader
 
 
-def parse_yaml(file: TextIOWrapper) -> Dict[Any, Any]:
+def parse_yaml(file: TextIOWrapper) -> Dict[str, Any]:
     return yaml.load(file, Loader=yaml.SafeLoader)
 
 
-def parse_json(file: TextIOWrapper) -> Dict[Any, Any]:
+def parse_yaml_from_path(path: str):
+    file = reader.get_read_file_obj(path)
+    return parse_yaml(file)
+
+
+def parse_json(file: TextIOWrapper) -> Dict[str, Any]:
+    return json.load(file)
+
+
+def parse_json_from_path(path: str):
+    file = reader.get_read_file_obj(path)
     return json.load(file)
