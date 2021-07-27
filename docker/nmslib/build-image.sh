6b483801c2d14882ba187a199ead6b023b22eac7
Sean Ye <yesean@amazon.com>
2021-07-26 17:26:06 -0700

    Add scripts for building images

---
+++ docker/nmslib/build-image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -f docker/nmslib/Dockerfile -t okpt/nmslib .
---
+++ docker/opensearch/build-image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -f docker/opensearch/Dockerfile -t okpt/opensearch .
