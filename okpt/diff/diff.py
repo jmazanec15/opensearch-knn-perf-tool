c98bb5c074349b729c17a32791c919e8f15468d1
Sean Ye <yesean@amazon.com>
2021-09-09 11:39:03 -0700

    Consolidate exceptions

--- okpt/diff/diff.py
+++ okpt/diff/diff.py
@@ -19,39 +19,14 @@
 from typing import Any, Dict, Tuple
 
 
-class InconsistentTestResultsError(Exception):
-    """Exception raised when two test results have different keys.
+class InvalidTestResultsError(Exception):
+    """Exception raised when the test results are invalid.
 
-    Attributes:
-        message -- explanation of the error
+    The results can be invalid if they have different fields, non-numeric
+    values, or if they don't follow the standard result format.
     """
-
-    def __init__(self, key: str, result: str):
-        self.message = f'key `{key}` is not present in {result}.'
-        super().__init__(self.message)
-
-
-class InvalidTestResultTypeError(Exception):
-    """Exception raised when a test result has non-numeric test result values.
-
-    Attributes:
-        message -- explanation of the error
-    """
-
-    def __init__(self, key: str, result: str):
-        self.message = f'key `{key}` in {result} points to a non-numeric value.'
-        super().__init__(self.message)
-
-
-class InvalidTestResultError(Exception):
-    """Exception raised when a test result has missing or invalid fields.
-
-    Attributes:
-        message -- explanation of the error
-    """
-
-    def __init__(self, key: str, result: str):
-        self.message = f'{result} has a missing or invalid key `{key}`.'
+    def __init__(self, msg: str):
+        self.message = msg
         super().__init__(self.message)
 
 
@@ -65,39 +40,55 @@ class Diff:
     Methods:
         diff: Returns the diff between two test results. (changed - base)
     """
-
-    def __init__(self, base_result: Dict[str, Any],
-                 changed_result: Dict[str, Any], metadata: bool):
+    def __init__(
+        self,
+        base_result: Dict[str,
+                          Any],
+        changed_result: Dict[str,
+                             Any],
+        metadata: bool
+    ):
         """Initializes test results and validate them."""
         self.base_result = base_result
         self.changed_result = changed_result
         self.metadata = metadata
 
-        # validate test result keys
+        # make sure results have proper test result fields
         is_valid, key, result = self._validate_keys()
         if not is_valid:
-            raise InvalidTestResultError(key, result)
+            raise InvalidTestResultsError(
+                f'{result} has a missing or invalid key `{key}`.'
+            )
 
         self.base_results = self.base_result['results']
         self.changed_results = self.changed_result['results']
 
-        # validate test result structure and types
+        # make sure results have the same fields
         is_valid, key, result = self._validate_structure()
         if not is_valid:
-            raise InconsistentTestResultsError(key, result)
+            raise InvalidTestResultsError(
+                f'key `{key}` is not present in {result}.'
+            )
+
+        # make sure results have numeric values
         is_valid, key, result = self._validate_types()
         if not is_valid:
-            raise InvalidTestResultTypeError(key, result)
+            raise InvalidTestResultsError(
+                f'key `{key}` in {result} points to a non-numeric value.'
+            )
 
     def _validate_keys(self) -> Tuple[bool, str, str]:
         """Ensure both test results have `metadata` and `results` keys."""
         check_keydict = lambda key, res: key in res and isinstance(
             res[key], dict)
+
+        # check if results have a `metadata` field and if `metadata` is a dict
         if self.metadata:
             if not check_keydict('metadata', self.base_result):
                 return (False, 'metadata', 'base_result')
             if not check_keydict('metadata', self.changed_result):
                 return (False, 'metadata', 'changed_result')
+        # check if results have a `results` field and `results` is a dict
         if not check_keydict('results', self.base_result):
             return (False, 'results', 'base_result')
         if not check_keydict('results', self.changed_result):
